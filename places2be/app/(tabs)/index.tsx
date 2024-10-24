7987311cfaa927a90868710a4dd845917d40ef54
mkphung29 <mkp6075@nyu.edu>
2024-10-23 20:50:23 -0400

    Set up expo on branch

---
+++ places2be/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+        <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
