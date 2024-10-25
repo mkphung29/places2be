f8d57149d0f6b6a29d494d366dfe585e019565d0
mkphung29 <mkp6075@nyu.edu>
2024-10-25 17:59:44 -0400

    Set up project structure

---
+++ places2be/app/(main)/Place.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+const place = () => {
+  return (
+    <div>place</div>
+  )
+}
+
+export default place
